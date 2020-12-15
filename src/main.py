6158ba224e430be444582c5b13c8813a41ef7a8f
Lukas Fam <takeruadelbert@gmail.com>
2020-12-15 13:58:23 +0700

    changed web server structure from using aiohttp into fastapi

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import os
 import re
@@ -6,7 +7,7 @@ from logging.handlers import TimedRotatingFileHandler
 import uvicorn
 from fastapi import FastAPI
 
-from src.misc.constant.value import DEFAULT_PORT, DEFAULT_APP_NAME
+from src.misc.constant.value import DEFAULT_PORT, DEFAULT_APP_NAME, DEFAULT_DELAY_TIME_CONVERT_STREAM_TO_FRAME_SCHEDULER
 from src.misc.helper.helper import create_log_dir_if_does_not_exists
 from src.model.model import EncodedUpload, UrlUpload
 from src.service import ConvertStreamToFrameService
@@ -43,5 +44,19 @@ async def upload_url(urlUpload: UrlUpload):
     return await service.upload_url(urlUpload)
 
 
+@app.on_event("startup")
+async def startup():
+    asyncio.ensure_future(scheduler_convert_stream_to_frame())
+
+
+async def scheduler_convert_stream_to_frame():
+    logger.info('starting scheduler convert stream to frame ...')
+    while True:
+        await service.auto_convert()
+        await asyncio.sleep(
+            int(os.getenv("DELAY_TIME_CONVERT_STREAM_TO_FRAME_SCHEDULER",
+                          DEFAULT_DELAY_TIME_CONVERT_STREAM_TO_FRAME_SCHEDULER)))
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, port=int(os.getenv('PORT', DEFAULT_PORT)))
+    uvicorn.run(app, port=int(os.getenv('PORT', DEFAULT_PORT)), host='0.0.0.0')
